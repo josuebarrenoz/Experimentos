470d8fd9c573860a9fb1c891a2ddf96ebcd9a493
Josue Barreno <maharba.josue@gmail.com>
2022-05-02 11:48:18 -0400

    Modularizando el conversor de monedas

--- codigo_python/funciones.py
+++ codigo_python/funciones.py
@@ -7,19 +7,27 @@
 #imprimir_mensaje()
 
 
-def conversacion(mensaje):
-    print("Hola")
-    print("Como estás")
-    print(mensaje)
-    print("Adios")
+# def conversacion(mensaje):
+#     print("Hola")
+#     print("Como estás")
+#     print(mensaje)
+#     print("Adios")
 
 
-opcion = int(input("Elige unaa opción (1,2,3): "))
-if opcion==1:
-    conversacion("Elegiste la opción 1")
-elif opcion==2:
-    conversacion("Elegiste la opción 2")
-elif opcion==3:
-    conversacion("Elegiste la opción 3")
-else:
-    print("Escribe la opción correcta ")
+# opcion = int(input("Elige unaa opción (1,2,3): "))
+# if opcion==1:
+#     conversacion("Elegiste la opción 1")
+# elif opcion==2:
+#     conversacion("Elegiste la opción 2")
+# elif opcion==3:
+#     conversacion("Elegiste la opción 3")
+# else:
+#     print("Escribe la opción correcta ")
+
+def suma (a, b):
+    print("se suman dos números")
+    resultado= a+b
+    return resultado
+
+sumatoria = suma(1,4)
+print(sumatoria)
