a3801cf79b839463e7f35aa0cce4d0c2eb22dff5
Josue Barreno <maharba.josue@gmail.com>
2022-05-02 05:34:47 -0400

    Experimentando un conversor de entrada multiple

--- codigo_python/conversor.py
+++ codigo_python/conversor.py
@@ -1,29 +1,38 @@
-#Input del usuario
-pesoscol = input("¿Cuantos pesos colombianos tienes?  ")
-pesoscol = float(pesoscol)
-#Declaracion de valores
-valor_dolar = 4000
-valor_euro = 4165.95
-valor_cake = 30632.08
-valor_btc = 29000000
-#Calculo valor dolar
-dolares= pesoscol / valor_dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
-#Calculo Valor Euro
-euros= pesoscol / valor_euro
-euros = round(euros, 2)
-euros = str(euros)
-#Calculo Cantidad de CAKE
-cakes = pesoscol / valor_cake
-cakes = round(cakes, 4)
-cakes = str(cakes)
-#Calculo Bitcoin
-btcs = pesoscol / valor_btc
-btcs = round(btcs, 9)
-btcs = str(btcs)
-#Prints
-print("Tienes $"+ dolares + " dolares")
-print("Tienes $"+ euros + " Euros")
-print("Tienes: " + cakes + " CAKE")
-print("Tienes: " + btcs + " Bitcoins")
+def exchanges(moneda,cantidad):
+    result = 0
+    # Moneda chilena
+    if moneda == 1:
+        result = cantidad * 0.0013
+        print(f'Los {cantidad} pesos chilenos equivalen a {result} dolares')
+    # Moneda colombiana
+    elif moneda == 2:
+        result = cantidad * 0.00027
+        print(f'Los {cantidad} pesos colombianos equivalen a {result} dolares')
+    # Moneda Argentina
+    elif moneda == 3:
+        result = cantidad * 0.014
+        print(f'Los {cantidad} pesos argentinos equivalen a {result} dolares')
+    # Moneda mexicana
+    elif moneda == 4:
+        result = cantidad * 0.044
+        print(f'Los {cantidad} pesos mexicanos equivalen a {result} dolares')
+    # Otro
+    else:
+        print('Ingresa solo un numero de la lista')
+
+
+if __name__ == '__main__':
+    try:
+        moneda = int(input('''
+        Ingresa el indice de la moneda que quieres convertira  dolar:
+            [1] Moneda chilena a Dolar
+            [2] Moneda colombiana a Dolar
+            [3] Moneda argentida a Dolar
+            [4] Moneda mexicana a Dolar
+        Selecciona: '''))
+        print('********************************')
+        cantidad = int(input('Ingresa la cantidad que quieres convertir: '))
+        exchanges(moneda,cantidad)
+    except:
+        print('* * * * * * E R R O R * * * * * *')
+        print('Por favor, Ingresa solo valores numericos')
