a784d848fc0f7e250a0ba9e5df26dac923d5b08d
Josue Barreno <maharba.josue@gmail.com>
2022-05-02 05:59:32 -0400

    Conversor multimoneda de 4 entradas y 3 salidas

--- codigo_python/conversor.py
+++ codigo_python/conversor.py
@@ -1,38 +1,82 @@
+#Declaracion de valores
+valor_dolar1 = 4.48071
+valor_euro1 = 4.72458
+valor_btc1 = 174133.833
+valor_dolar2 = 3958
+valor_euro2 = 4165.18
+valor_btc2 = 153673703.8
+valor_dolar3 = 115.32
+valor_euro3 = 121.37
+valor_btc3 = 4477406.44
+valor_dolar4 = 20.4
+valor_euro4 = 21.47
+valor_btc4 = 792178.24
+
 def exchanges(moneda,cantidad):
     result = 0
-    # Moneda chilena
+    # Moneda venezolana
     if moneda == 1:
-        result = cantidad * 0.0013
-        print(f'Los {cantidad} pesos chilenos equivalen a {result} dolares')
+        result = round (cantidad / valor_dolar1,2)
+        result1 = round(cantidad / valor_euro1,2)
+        result2 = round(cantidad / valor_btc1,9)
+        print(f' ')
+        print(f'Los {cantidad} Bolivares equivalen a:')
+        print(f' ')
+        print(f'{result} dolares')
+        print(f'{result1} euros')
+        print(f'{result2} bitcoin')
     # Moneda colombiana
     elif moneda == 2:
-        result = cantidad * 0.00027
-        print(f'Los {cantidad} pesos colombianos equivalen a {result} dolares')
+        result = round (cantidad / valor_dolar2,2)
+        result1 = round(cantidad / valor_euro2,2)
+        result2 = round(cantidad / valor_btc2,9)
+        print(f' ')
+        print(f'Los {cantidad} pesos colombianos equivalen a:')
+        print(f' ')
+        print(f'{result} dolares')
+        print(f'{result1} euros')
+        print(f'{result2} bitcoin')
     # Moneda Argentina
     elif moneda == 3:
-        result = cantidad * 0.014
-        print(f'Los {cantidad} pesos argentinos equivalen a {result} dolares')
+        result = round (cantidad / valor_dolar3,2)
+        result1 = round(cantidad / valor_euro3,2)
+        result2 = round(cantidad / valor_btc3,9)
+        print(f' ')
+        print(f'Los {cantidad} pesos argentinos equivalen a:')
+        print(f' ')
+        print(f'{result} dolares')
+        print(f'{result1} euros')
+        print(f'{result2} bitcoin')
     # Moneda mexicana
     elif moneda == 4:
-        result = cantidad * 0.044
-        print(f'Los {cantidad} pesos mexicanos equivalen a {result} dolares')
+        result = round (cantidad / valor_dolar4,2)
+        result1 = round(cantidad / valor_euro4,2)
+        result2 = round(cantidad / valor_btc4,9)
+        print(f' ')
+        print(f'Los {cantidad} pesos mexicanos equivalen a:')
+        print(f' ')
+        print(f'{result} dolares')
+        print(f'{result1} euros')
+        print(f'{result2} bitcoin')
     # Otro
     else:
+        print(f' ')
         print('Ingresa solo un numero de la lista')
 
 
 if __name__ == '__main__':
     try:
         moneda = int(input('''
-        Ingresa el indice de la moneda que quieres convertira  dolar:
-            [1] Moneda chilena a Dolar
-            [2] Moneda colombiana a Dolar
-            [3] Moneda argentida a Dolar
-            [4] Moneda mexicana a Dolar
+        Ingresa el indice de la moneda que quieres convertir a  dolares, euros o bitcoin:
+            [1] Moneda venezolana
+            [2] Moneda colombiana 
+            [3] Moneda argentida 
+            [4] Moneda mexicana 
         Selecciona: '''))
         print('********************************')
         cantidad = int(input('Ingresa la cantidad que quieres convertir: '))
         exchanges(moneda,cantidad)
     except:
+        print(f' ')
         print('* * * * * * E R R O R * * * * * *')
         print('Por favor, Ingresa solo valores numericos')
