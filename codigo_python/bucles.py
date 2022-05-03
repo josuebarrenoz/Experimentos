be46fa2ee731466390588c4028284937599089e8
Josue Barreno <maharba.josue@gmail.com>
2022-05-03 18:19:46 -0400

    Jugando con funciones condicionadas recursivas

--- codigo_python/bucles.py
+++ codigo_python/bucles.py
@@ -1,2 +1,12 @@
-contador = 0
-print("2 elevado a "+str(contador)+ " es igual a: "+str(2**contador))
+def cont(v1,v2):
+    if v1<v2+1:
+        print("2 elevado a "+str(v1)+ " es igual a: "+str(2**v1))     
+        cont(v1+1,v2)
+    else:
+        print("Fin!")
+
+
+if __name__ == "__main__":
+    v2 = int(input("Cuantas potencias de dos quieres calcular: "))
+    v1=0
+    cont(v1,v2)
