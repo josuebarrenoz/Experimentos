d75dc56bb6dc45866510643cfc6a7ed2df0a1f3d
Josue Barreno <maharba.josue@gmail.com>
2022-05-04 15:43:41 -0400

    Hacer un desafio que ví en tweeter

---
+++ codigo_python/Desafio_ciclos.py
@@ -0,0 +1,15 @@
+def menu():
+    n = str(input(""" 
+    Ingrese un numero:  """))
+    return
+
+
+def run():
+    x=0
+    y=0
+    for i in range(0,menu):
+        pass
+        
+    
+if __name__ == "__main__":
+    run()
