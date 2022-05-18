354adbd4fecc6f208ffbd9b67fea47e6958cffd1
Josue Barreno <maharba.josue@gmail.com>
2022-05-18 10:43:10 -0400

    Objetos, Dando vida a las clases en java Y python

--- poo/java/Car.java
+++ poo/java/Car.java
@@ -3,4 +3,10 @@ class Car {
     String license;
     String driver;
     Integer passegenger;
+
+    void printDataCar(){
+        System.out.println("License: " + license + ", Driver: " + driver);
+
+    };
+
 }
