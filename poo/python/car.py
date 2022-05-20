cfc3ebf096a7bb604ddbb73d967385f4ead8dbc5
Josue Barreno <maharba.josue@gmail.com>
2022-05-20 12:27:58 -0400

    Declarando un método constructor en Python

--- poo/python/car.py
+++ poo/python/car.py
@@ -1,8 +1,11 @@
-from lib2to3.pgen2 import driver
-
+from account import Account
 
 class Car:
     id          = int
     license     = str
-    driver      = str
+    driver      =  Account("","")
     passegenger = int
+
+    def __init__(self,license, driver):
+        self.license = license
+        self.driver = driver
