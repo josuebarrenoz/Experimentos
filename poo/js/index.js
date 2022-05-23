325483654aabbb8cefa15be4b266e5dd01892550
Josue Barreno <maharba.josue@gmail.com>
2022-05-23 15:11:52 -0400

    Aplicando herencia en lenguaje Python y JavaScript

--- poo/js/index.js
+++ poo/js/index.js
@@ -1,5 +1,7 @@
-var car =new Car("AW456",new Account("Andres Herrera","QWE234"))
+var car = new Car("AW456",new Account("Andres Herrera","QWE234"))
 car.passenger =4;
 car.printDataCar();
 
-
+var Uber = new UberX("AW456",new Account("Andrea Ferram","ANDA765"),"Chevrolet","Spark")
+UberX.passenger =4;
+UberX.printDataCar();
